806fbb71478f54b517fc97ed919de7a905562e75
gwnBallesteros <gwenballesteros.dev@gmail.com>
2022-06-24 11:02:34 +0800

    Add project files.

---
+++ frmFurnitureApplication.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFurnitureApplication
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmFurnitureApplication))
+        Me.picFurniture = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblSalesAmount = New System.Windows.Forms.Label()
+        Me.TxtSales = New System.Windows.Forms.TextBox()
+        Me.lblSalesTax = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblTaxRate = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picFurniture, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'picFurniture
+        '
+        Me.picFurniture.Image = Global.Furniture_Application.My.Resources.Resources.toa_heftiba_FV3GConVSss_unsplash
+        Me.picFurniture.Location = New System.Drawing.Point(-2, 30)
+        Me.picFurniture.Name = "picFurniture"
+        Me.picFurniture.Size = New System.Drawing.Size(356, 421)
+        Me.picFurniture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picFurniture.TabIndex = 0
+        Me.picFurniture.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Bisque
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 30)
+        Me.Panel1.TabIndex = 1
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(505, 45)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(162, 28)
+        Me.lblHeading.TabIndex = 2
+        Me.lblHeading.Text = "Sales Calculator"
+        '
+        'lblSalesAmount
+        '
+        Me.lblSalesAmount.AutoSize = True
+        Me.lblSalesAmount.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSalesAmount.Location = New System.Drawing.Point(25, 22)
+        Me.lblSalesAmount.Name = "lblSalesAmount"
+        Me.lblSalesAmount.Size = New System.Drawing.Size(126, 25)
+        Me.lblSalesAmount.TabIndex = 3
+        Me.lblSalesAmount.Text = "Sales Amount:"
+        '
+        'TxtSales
+        '
+        Me.TxtSales.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtSales.Location = New System.Drawing.Point(223, 26)
+        Me.TxtSales.Name = "TxtSales"
+        Me.TxtSales.Size = New System.Drawing.Size(100, 31)
+        Me.TxtSales.TabIndex = 4
+        '
+        'lblSalesTax
+        '
+        Me.lblSalesTax.AutoSize = True
+        Me.lblSalesTax.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSalesTax.Location = New System.Drawing.Point(25, 75)
+        Me.lblSalesTax.Name = "lblSalesTax"
+        Me.lblSalesTax.Size = New System.Drawing.Size(90, 25)
+        Me.lblSalesTax.TabIndex = 5
+        Me.lblSalesTax.Text = "Sales Tax: "
+        '
+        'lblTax
+        '
+        Me.lblTax.AutoSize = True
+        Me.lblTax.BackColor = System.Drawing.Color.White
+        Me.lblTax.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTax.Location = New System.Drawing.Point(218, 75)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(76, 25)
+        Me.lblTax.TabIndex = 6
+        Me.lblTax.Text = "$100.00"
+        '
+        'lblTaxRate
+        '
+        Me.lblTaxRate.AutoSize = True
+        Me.lblTaxRate.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTaxRate.Location = New System.Drawing.Point(533, 73)
+        Me.lblTaxRate.Name = "lblTaxRate"
+        Me.lblTaxRate.Size = New System.Drawing.Size(106, 25)
+        Me.lblTaxRate.TabIndex = 7
+        Me.lblTaxRate.Text = "5% Tax Rate"
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.AutoSize = True
+        Me.lblTotalDue.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalDue.Location = New System.Drawing.Point(25, 127)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(95, 25)
+        Me.lblTotalDue.TabIndex = 8
+        Me.lblTotalDue.Text = "Total Due: "
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.BackColor = System.Drawing.Color.White
+        Me.lblTotal.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(218, 127)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(90, 25)
+        Me.lblTotal.TabIndex = 9
+        Me.lblTotal.Text = "$1,000.00"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.GroupBox1.Controls.Add(Me.lblSalesAmount)
+        Me.GroupBox1.Controls.Add(Me.lblTotal)
+        Me.GroupBox1.Controls.Add(Me.TxtSales)
+        Me.GroupBox1.Controls.Add(Me.lblTotalDue)
+        Me.GroupBox1.Controls.Add(Me.lblSalesTax)
+        Me.GroupBox1.Controls.Add(Me.lblTax)
+        Me.GroupBox1.Location = New System.Drawing.Point(393, 118)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(369, 185)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.Bisque
+        Me.btnCalculate.Location = New System.Drawing.Point(380, 339)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(182, 37)
+        Me.btnCalculate.TabIndex = 11
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Bisque
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Location = New System.Drawing.Point(595, 339)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(182, 37)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Bisque
+        Me.btnExit.Location = New System.Drawing.Point(488, 388)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(182, 37)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'frmFurnitureApplication
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblTaxRate)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.picFurniture)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmFurnitureApplication"
+        Me.Text = "Furniture Application"
+        CType(Me.picFurniture, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picFurniture As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblSalesAmount As Label
+    Friend WithEvents TxtSales As TextBox
+    Friend WithEvents lblSalesTax As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblTaxRate As Label
+    Friend WithEvents lblTotalDue As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ frmFurnitureApplication.vb
@@ -0,0 +1,43 @@
+﻿'Name of The Program       Sales Calculator Furniture Application
+'Developer                 G.T. Ballesteros
+'Date                      June 24, 2022
+'Purpose                   Calculate and Display the Sales Tax and Total due amounts.
+
+Option Strict On
+Public Class frmFurnitureApplication
+
+    Const _cdecTaxRate = 0.05D
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intSales As Integer
+        Dim decTax As Decimal
+        Dim decTotalSales As Decimal
+
+        'Calculate the Tax
+        Integer.TryParse(TxtSales.Text, intSales)
+        decTax = intSales * _cdecTaxRate
+        lblTax.Text = decTax.ToString("C2")
+
+        'Calculate the Total Due Amount
+        decTotalSales = intSales + decTax
+        lblTotal.Text = decTotalSales.ToString("C2")
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        TxtSales.Clear()
+        lblTax.Text = ""
+        lblTotal.Text = ""
+        TxtSales.Focus()
+    End Sub
+
+    Private Sub frmFurnitureApplication_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblTaxRate.Text = "5% Tax Rate"
+        lblTax.Text = ""
+        lblTotal.Text = ""
+        TxtSales.Focus()
+    End Sub
+End Class
